1c9888da3164ba13895444ea0b1011decf0b6ca6
krasnoperov alexandr <immnsn.k@gmail.com>
2023-07-02 19:53:59 +0300

    eslint

--- server.ts
+++ server.ts
@@ -11,13 +11,12 @@ const PORT = 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use('/images',express.static(__dirname + '/src/statics'));
+app.use("/images", express.static(__dirname.concat("/src/statics")));
 app.use(express.static("./dist"));
 app.get("*", (req, res) => {
   res.sendFile("./dist/index.html", { root: __dirname });
 });
-console.log(__dirname)
-
+console.log(__dirname);
 
 app.use((req, res) => {
   res.status(404).send("./dist");
